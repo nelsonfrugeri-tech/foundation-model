702af88a6f8e13ce239e41313ed0151df1b48a86
Nelson Frugeri Jr <nfrugeri@gmail.com>
2024-04-23 10:29:57 -0300

    Implemented env var to the bedrock

--- src/foundation_model/provider.py
+++ src/foundation_model/provider.py
@@ -3,6 +3,7 @@ from modelhub.api.adapter.http.v1.model.request.text_request import TextRequest
 from foundation_model.openai.adapter.openai_adapter import OpenAIAdapter
 from foundation_model.antrhopic.adapter.anthropic_adapter import AnthropicAdapter
 from foundation_model.bedrock.adapter.bedrock_adapter import BedrockAdapter
+from foundation_model.port.interface_port import InterfacePort
 
 
 class ProviderHub:
@@ -13,5 +14,7 @@ class ProviderHub:
             "bedrock": BedrockAdapter(),
         }
 
-    def generate_text(self, provider_name: str, text_request_body: TextRequest):
+    def generate_text(
+        self, provider_name: str, text_request_body: TextRequest
+    ) -> InterfacePort:
         return self.providers[provider_name].generate_text(text_request_body)
